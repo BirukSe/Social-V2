ebe7f98468557a6492e6d61287a59294b806a863
biruke <93684688+BirukSe@users.noreply.github.com>
2025-02-28 09:26:14 +0300

    Update page.tsx

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Feed from '@/components/Feed';
 import dynamic from 'next/dynamic';
+import { CarouselProps } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
+import { CarouselState, AnimationHandlerResponse } from 'react-responsive-carousel/lib/ts/components/Carousel/types';
 
 const Carousel = dynamic(
   () => import('react-responsive-carousel').then(mod => mod.Carousel),
@@ -20,8 +22,39 @@ const Page = () => {
           interval={3000} // 3 seconds between slides
           showThumbs={false} // Hide thumbnails
           showStatus={false} // Hide status (e.g., "1 of 3")
-          className="w-full h-full"
-        >
+          className="w-full h-full" axis={'horizontal'} centerSlidePercentage={0} labels={{
+            leftArrow: '',
+            rightArrow: '',
+            item: ''
+          }} onClickItem={function (index: number, item: React.ReactNode): void {
+            throw new Error('Function not implemented.');
+          } } onClickThumb={function (index: number, item: React.ReactNode): void {
+            throw new Error('Function not implemented.');
+          } } onChange={function (index: number, item: React.ReactNode): void {
+            throw new Error('Function not implemented.');
+          } } onSwipeStart={function (event: React.TouchEvent): void {
+            throw new Error('Function not implemented.');
+          } } onSwipeEnd={function (event: React.TouchEvent): void {
+            throw new Error('Function not implemented.');
+          } } onSwipeMove={function (event: React.TouchEvent): boolean {
+            throw new Error('Function not implemented.');
+          } } preventMovementUntilSwipeScrollTolerance={false} renderArrowPrev={function (clickHandler: () => void, hasPrev: boolean, label: string): React.ReactNode {
+            throw new Error('Function not implemented.');
+          } } renderArrowNext={function (clickHandler: () => void, hasNext: boolean, label: string): React.ReactNode {
+            throw new Error('Function not implemented.');
+          } } renderIndicator={function (clickHandler: (e: React.MouseEvent | React.KeyboardEvent) => void, isSelected: boolean, index: number, label: string): React.ReactNode {
+            throw new Error('Function not implemented.');
+          } } renderItem={function (item: React.ReactNode, options?: { isSelected: boolean; isPrevious: boolean; }): React.ReactNode {
+            throw new Error('Function not implemented.');
+          } } renderThumbs={function (children: React.ReactChild[]): React.ReactChild[] {
+            throw new Error('Function not implemented.');
+          } } selectedItem={0} showArrows={false} showIndicators={false} statusFormatter={function (currentItem: number, total: number): string {
+            throw new Error('Function not implemented.');
+          } } stopOnHover={false} swipeable={false} swipeScrollTolerance={0} transitionTime={0} verticalSwipe={'natural'} width={''} animationHandler={'slide'} swipeAnimationHandler={function (delta: { x: number; y: number; }, props: CarouselProps, state: CarouselState, setState: Function): AnimationHandlerResponse {
+            throw new Error('Function not implemented.');
+          } } stopSwipingHandler={function (props: CarouselProps, state: CarouselState): AnimationHandlerResponse {
+            throw new Error('Function not implemented.');
+          } }        >
           <div>
             <img src="ai.jpg" alt="Heart" className="h-[300px]" />
             <p className="legend">Legend 1</p>
